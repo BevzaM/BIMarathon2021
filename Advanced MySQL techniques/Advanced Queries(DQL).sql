883f260449f17efa789dac391cb75acfdd5a7596
BevzaM <97997728+BevzaM@users.noreply.github.com>
2022-03-26 23:13:05 -0500

    Update

--- Advanced MySQL techniques/Advanced Queries(DQL).sql
+++ Advanced MySQL techniques/Advanced Queries(DQL).sql
@@ -1,11 +1,10 @@
+-- create database BI_Marathon_IBRD
 
- 
-  
- -- create database BI_Marathon_IBRD
   create database BI_Marathon_IBRD;
-Use BI_Marathon_IBRD;
+  Use BI_Marathon_IBRD;
 
 -- Creating a temp table to loud our CSV file
+
 create table IBRD_table_temp
 (
 Region varchar(36)
@@ -41,6 +40,7 @@ Region varchar(36)
 );
 
  -- check table from Null values
+ 
   Select *
 From ibrd_table_temp
 Where 
@@ -75,25 +75,25 @@ Effective_Date is null or
 Close_Date is null or
 Last_Disbursement_Date is null;
 
-
--- ONLY Use this script If you need to truncate your table (remove the values in your table)
+-- ONLY Use this script If you need remove the values in your table
 truncate ibrd_table_temp;
 
 -- select all rows from tem_table
 select * 
-from ibrd_table_temp
-;
+from ibrd_table_temp;
+
 -- create #1 dimention "Dim_Location"
+
 create table Dim_Location (
 	Location_ID int not null auto_increment
     ,Region varchar(36)
     ,Country varchar(36)
     ,Country_Code varchar (3)
     ,primary key (Location_ID)
-    )
- ;
+    );
 
 -- create #2 dimention "Dim_Loan"
+
 create table Dim_Loan
 (
 	 Loan_Number varchar(36) not null
@@ -105,9 +105,10 @@ create table Dim_Loan
     ,Guarantor varchar(72)
     ,Guarantor_Country_Code varchar(3)
     ,primary key (Loan_Number)
-    )
-;
+    );
+    
 -- create #3 "Record_of_loans" table
+
 create table Record_of_loans
 (
 	Record_ID int not null auto_increment
@@ -135,19 +136,22 @@ create table Record_of_loans
     , primary key (Record_ID)
     , FOREIGN KEY (Location_ID) REFERENCES dim_location (Location_ID) ON DELETE SET NULL
     , FOREIGN KEY (Loan_Number) REFERENCES Dim_Loan (Loan_Number) ON DELETE SET NULL
-    )
-;
+    );
+    
 -- uploading #1 "dim_loan" table
 INSERT IGNORE INTO dim_loan (Loan_Number, Loan_Type, Loan_Status, Project_Name, Project_ID, Borrower, Guarantor, Guarantor_Country_Code)
-SELECT DISTINCT Loan_Number, Loan_Type, Loan_Status, Project_Name, Project_ID, Borrower, Guarantor, Guarantor_Country_Code FROM ibrd_table_temp
-;
+SELECT DISTINCT Loan_Number, Loan_Type, Loan_Status, Project_Name, Project_ID, Borrower, Guarantor, Guarantor_Country_Code FROM ibrd_table_temp;
+
+-- check result
 select *
-from dim_loan
-;
+from dim_loan;
+
 -- uploading #2 "dim_location" table
 INSERT IGNORE INTO dim_location (Region, Country, Country_Code)
-SELECT DISTINCT Region, Country, Country_Code FROM ibrd_table_temp
-;
+SELECT DISTINCT Region, Country, Country_Code 
+FROM ibrd_table_temp;
+
+-- check result
 select *
 from dim_location;
 
@@ -182,12 +186,13 @@ SELECT distinct
     , tmp.Last_Disbursement_Date
 FROM ibrd_table_temp AS tmp
 JOIN dim_loan ON dim_loan.Loan_Number = tmp.Loan_Number
-JOIN dim_location ON dim_location.Country_Code = tmp.Country_Code
-;
+JOIN dim_location ON dim_location.Country_Code = tmp.Country_Code;
 
 -- Check from duplicate Rows using Group By and Having clause in dim
+-- # record_of_loans
+
 SELECT 
-  Loan_Number
+     Loan_Number
     ,Original_Principal_Amount
     ,Undisbursed_Amount
     ,Disbursed_Amount
@@ -232,7 +237,7 @@ group by
     ,Last_Disbursement_Date
 Having Count(*) > 1;
 
-
+-- # dim_loan
 SELECT 
   Loan_Number
 , Loan_Type
@@ -255,7 +260,7 @@ group by
 , Guarantor_Country_Code
 Having Count(*) > 1;
 
-
+-- # dim_location
 SELECT 
   Region
 , Country
@@ -304,6 +309,7 @@ Having CNT > 1;
 
 
 -- Check from duplicate Rows using Common Table Expressions (CTE)
+
 With CTE (Location_ID, Region , Country, Country_Code, duplicatecount)
     AS (select Location_ID, Region , Country, Country_Code,
     ROW_NUMBER() OVER(partition BY Region , Country, Country_Code 
@@ -313,9 +319,8 @@ select *
  FROM CTE
  WHERE duplicatecount > 1;  
  
-
-
 -- Option : Remove Duplicate Rows Using ROW_NUMBER()
+
     DELETE FROM dim_location
     WHERE Location_ID 
     IN(  
@@ -327,16 +332,16 @@ select *
     WHERE row_num>1  
 );  
 
--- Use distinct keyword to see unic region
+-- Use distinct keyword to see unic Region
 select distinct Region
 From dim_location;
 
--- Using the "Case when" function to create a new column to understand information from a different angle
+-- Using the "Case when" function to create a new column to understand the number of countries in the "Continents" list.
 SELECT Count(*) as number_of_countries,
 Case 
 When Region in ('AFRICA EAST', 'AFRICA WEST', 'MIDDLE EAST AND NORTH AFRICA') Then 'Africa'
 When Region in ('EAST ASIA AND PACIFIC', 'EUROPE AND CENTRAL ASIA', 'SOUTH ASIA','OTHER') Then 'Eurasia'
-When Region = 'LATIN AMERICA AND CARIBBEAN' Then 'America'
+When Region = 'LATIN AMERICA AND CARIBBEAN' Then 'AMEC'
 End As 'continents'
 From dim_location
 Group By Continents;
@@ -356,18 +361,13 @@ Country_Code is null;
 
 -- change null from 'N/A'
 Select Location_ID, COALESCE(Region, 'N/A') , COALESCE(Country, 'N/A'), COALESCE(Country_Code, 'N/A')
-From dim_location
-Where Country is null;
+From dim_location;
 
 -- change null from 'N/A'
 update dim_location
 SET Country = COALESCE(Country, 'N/A'),
  Region = COALESCE(Region, 'N/A'), 
- Country_Code = COALESCE(Country_Code, 'N/A')
-Where 
-Country is null or 
-Region is null or
-Country_Code is null;
+ Country_Code = COALESCE(Country_Code, 'N/A');
 
 -- Check 'N/A'
 Select *
@@ -418,7 +418,7 @@ Where Region is null or
 Country is null or
 Country_Code is null;
 
--- Examples of MySQL GREATEST() and LEAST()
+-- Examples of MySQL GREATEST() and LEAST() if need change same values
 Select Record_ID, Loan_Number, GREATEST(Original_Principal_Amount, Undisbursed_Amount, Disbursed_Amount) as max_value,
  LEAST(Original_Principal_Amount, Undisbursed_Amount, Disbursed_Amount)  as min_value
  From record_of_loans;
@@ -431,7 +431,8 @@ Select Record_ID, Loan_Number, GREATEST(Original_Principal_Amount, Undisbursed_A
  From record_of_loans
  Order BY New_value desc;
  
--- Common Table Expressions (CTEs)
+-- USE Common Table Expressions (CTEs) to find data according to the business task
+
  With FIX_Location AS (
  SELECT Distinct Location_ID
  FROM dim_location
@@ -439,12 +440,61 @@ Select Record_ID, Loan_Number, GREATEST(Original_Principal_Amount, Undisbursed_A
  AVG_Amount_date AS (
  SELECT AVG(Original_Principal_Amount) AS AVG_Amount
  FROM record_of_loans
- WHERE date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%Y') between '2009' and '2010')
+ WHERE date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%Y') between '2020' and '2022')
  SELECT Location_ID, Original_Principal_Amount, Board_Approval_Date
  FROM record_of_loans
  WHERE Location_ID IN (SELECT Distinct Location_ID from FIX_Location)
- AND Original_Principal_Amount >= (SELECT Original_Principal_Amount from AVG_Amount_date)
+ AND Original_Principal_Amount >= (SELECT Original_Principal_Amount from AVG_Amount_date) AND
+ (date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%Y') between '2020' and '2022')
  ORDER BY Original_Principal_Amount desc;
  
- 
+ -- use this script if you need to find responses within a certain period of time and group the values
+SELECT Location_ID, 
+SUM(CASE WHEN date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%c/%Y') = '1/2021' THEN Original_Principal_Amount END) AS "JAN-2021",
+SUM(CASE WHEN date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%c/%Y') = '2/2021' THEN Original_Principal_Amount END) AS "FEB-2021",
+SUM(CASE WHEN date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%c/%Y') = '3/2021' THEN Original_Principal_Amount END) AS "MAR-2021"
+FROM record_of_loans
+WHERE date_format(str_to_date(Board_Approval_Date, '%c/%e/%Y'), '%c/%Y') BETWEEN  '01/2021' and '12/2021'
+Group BY 1;
+
+-- At this point, I suggest changing the data type for dates from varchar to date into a standard format for MySQL
+-- (if you want to use a script where the dates are still in varchar (text) format, update/load from ... like I did at the beginning)
+-- Pivoting Data w/ CASE WHEN
+update record_of_loans 
+set First_Repayment_Date = nullif(DATE_FORMAT(STR_TO_DATE(First_Repayment_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0')
+, Last_Repayment_Date = nullif(DATE_FORMAT(STR_TO_DATE(Last_Repayment_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0')
+, Agreement_Signing_Date = nullif(DATE_FORMAT(STR_TO_DATE(Agreement_Signing_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0') 
+, Board_Approval_Date = nullif(DATE_FORMAT(STR_TO_DATE(Board_Approval_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0')
+, Effective_Date = nullif(DATE_FORMAT(STR_TO_DATE(Effective_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0')
+, Close_Date = nullif(DATE_FORMAT(STR_TO_DATE(Close_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0') 
+, Last_Disbursement_Date = nullif(DATE_FORMAT(STR_TO_DATE(Last_Disbursement_Date, '%c/%e/%Y'),'%Y-%c-%e'), '0000-0-0');
+
+-- Check 'record_of_loans' date values
+SELECT First_Repayment_Date, Last_Repayment_Date, Agreement_Signing_Date, Board_Approval_Date, Effective_Date, Close_Date, Last_Disbursement_Date
+FROM bi_marathon_ibrd.record_of_loans;
+
+ALTER TABLE `bi_marathon_ibrd`.`record_of_loans` 
+CHANGE COLUMN `First_Repayment_Date` `First_Repayment_Date` DATE NULL DEFAULT NULL,
+CHANGE COLUMN `Last_Repayment_Date` `Last_Repayment_Date` DATE NULL DEFAULT NULL, 
+CHANGE COLUMN `Agreement_Signing_Date` `Agreement_Signing_Date` DATE NULL DEFAULT NULL, 
+CHANGE COLUMN `Board_Approval_Date` `Board_Approval_Date` DATE NULL DEFAULT NULL, 
+CHANGE COLUMN `Effective_Date` `Effective_Date` DATE NULL DEFAULT NULL, 
+CHANGE COLUMN `Close_Date` `Close_Date` DATE NULL DEFAULT NULL, 
+CHANGE COLUMN `Last_Disbursement_Date` `Last_Disbursement_Date` DATE NULL DEFAULT NULL;
+
+-- Recursive CTEs
+-- Self JOINS
+-- Window Functions
+SELECT name
+, GPA
+, row_number() OVER (ORDER BY GPA DESC)
+, RANK () OVER (ORDER BY GPA DESC)
+, DENSE_RANK() (ORDER BY GPA DESC);
+
+-- Calculating Running Totals
+Select month, Revenue, SUM(Revenue) OVER (ORDER BY MONTH) AS Comulative
+From monthly_REVENUE
+
+-- Delta Values
+
 
